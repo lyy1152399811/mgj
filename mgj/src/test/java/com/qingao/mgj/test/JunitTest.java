70f63695e78ffdc1bca59856a1b22d0bffda0565
lyy1152399811 <17864196968@163.com>
2019-11-25 22:43:10 +0800

    OK

--- mgj/src/test/java/com/qingao/mgj/test/JunitTest.java
+++ mgj/src/test/java/com/qingao/mgj/test/JunitTest.java
@@ -1,19 +1,32 @@
 package com.qingao.mgj.test;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.qingao.mgj.mapper.AreainfoMapper;
+import com.qingao.mgj.pojo.Areainfo;
+import com.qingao.mgj.pojo.AreainfoExample;
+
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootApplication(scanBasePackages = "com.qingao.mgj")
 public class JunitTest {
+	@Autowired
+	AreainfoMapper areainfoMapper;
 	@Test
 	public void test(){
-		System.out.println(1);
+		AreainfoExample example=new AreainfoExample();
+		List<Areainfo> list=areainfoMapper.selectByExample(example);
+		for (Areainfo areainfo : list) {
+			System.out.println(areainfo.getAname());
+		}
 	}
 
 }
